737e4372e3d5d381bd470e23103b90e3625a491b
Lemeshev Aleksandr <tivzrrr@gmail.com>
2021-06-11 17:34:52 +0300

    #2: remove hardcodes, translation error codes, refactoring dataFilm

--- parser/parser.py
+++ parser/parser.py
@@ -6,25 +6,29 @@ from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
 
+def delay():
+    time.sleep(2)
+
+
 def parsingTable(start, end):
     page = start
     while page < end:
-        time.sleep(2)
+        delay()
         responseTable = getResponseTable(page)
         if responseTable is None:
-            print("Err: нет интернета.")
+            print("Err: no internet connection.")
             continue
 
         soupTable = BeautifulSoup(responseTable.text, "lxml")
 
         films = getFilms(soupTable)
         if films is None:
-            print("Err: ошибка в парсинге страницы.")
+            print("Err: error in parsing the page.")
             continue
 
         lenFilms = len(films)
         if lenFilms == 0:
-            print("Err: страница не загрузилась.")
+            print("Err: the page didn't load.")
             continue
 
         print(f"on page {page} uploaded {lenFilms} films.")
@@ -32,7 +36,6 @@ def parsingTable(start, end):
         parsingFilm(lenFilms, idsFilms, valuesFilms)
 
         page += 1
-        time.sleep(2)
 
 
 def getResponseTable(currentPage):
@@ -65,7 +68,7 @@ def filmsHandler(listFilms):
 def parsingFilm(len, ids, values):
     i = 0
     while i < len:
-        time.sleep(2)
+        delay()
         if values[i] == "—":
             i += 1
             continue
@@ -74,29 +77,29 @@ def parsingFilm(len, ids, values):
         dataFilm = {
             "id": ids[i][6:-1],
             "type": "1",
-            "title": "—",
-            "originalTitle": "—",
-            "year": "—",
-            "country": "—",
-            "director": "—",
-            "budget": "—",
-            "runtime": "—",
-            "worldGross": "—",
-            "genres": "—",
-            "age": "—",
-            "actors": "—",
-            "description": "—",
-            "image": "—",
-            "imageUrl": "—",
-            "rating": "—",
-            "count": "—",
-            "ratingImdb": "—",
-            "countImdb": "—"
+            "title": None,
+            "originalTitle": None,
+            "year": None,
+            "country": None,
+            "director": None,
+            "budget": None,
+            "runtime": None,
+            "worldGross": None,
+            "genres": None,
+            "age": None,
+            "actors": None,
+            "description": None,
+            "image": None,
+            "imageUrl": None,
+            "rating": None,
+            "count": None,
+            "ratingImdb": None,
+            "countImdb": None
         }
 
         responseFilm = getResponseFilm(linkFilm)
         if responseFilm is None:
-            print("Err: нет интернета.")
+            print("Err: no internet connection.")
             continue
 
         soupFilm = BeautifulSoup(responseFilm.text, "lxml")
@@ -105,23 +108,22 @@ def parsingFilm(len, ids, values):
         if title is not None:
             dataFilm["title"] = title
         else:
-            print("Err: фильм не загрузился.")
+            print("Err: the movie didn't load.")
             continue
 
         originalTitle = getOriginalTitleFilm(soupFilm)
         if originalTitle is not None:
             dataFilm["originalTitle"] = originalTitle
         else:
-            print("War: нет original title.")
+            print("War: no original title.")
 
         encyclopedicData = getEncyclopedicDataFilm(soupFilm)
-        print(encyclopedicData)
         if encyclopedicData is not None:
             for key in encyclopedicData.keys():
                 if encyclopedicData[key] is not None:
                     dataFilm[key] = encyclopedicData[key]
         else:
-            print("War: нет encyclopedic.")
+            print("War: no encyclopedic.")
 
         if "сезон" in dataFilm["year"]:
             dataFilm["type"] = "0"
@@ -130,36 +132,36 @@ def parsingFilm(len, ids, values):
         if actors is not None:
             dataFilm["actors"] = actors
         else:
-            print("War: нет actors.")
+            print("War: no actors.")
 
         description = getDescriptionFilm(soupFilm)
         if description is not None:
             dataFilm["description"] = description
         else:
-            print("War: нет информации о фильме.")
+            print("War: no description.")
 
         image, imageUrl = getImageFilm(soupFilm, dataFilm["id"])
         if image is not None:
             dataFilm["image"] = image
             dataFilm["imageUrl"] = imageUrl
         else:
-            print("War: нет постера к фильму.")
+            print("War: no image or not found folder img-data.")
 
         rating, count = getRatingFilm(soupFilm)
         if rating is not None:
             dataFilm["rating"] = rating
             dataFilm["count"] = count
         else:
-            print("War: Нет оценки.")
+            print("War: no rating.")
 
         ratingImdb, countImdb = getRatingImdbFilm(soupFilm)
         if ratingImdb is not None:
             dataFilm["ratingImdb"] = ratingImdb
             dataFilm["countImdb"] = countImdb
         else:
-            print("War: Нет оценки imdb.")
+            print("War: no imdb rating.")
 
-        print(i + 1, dataFilm)
+        print(dataFilm)
         i += 1
 
 
@@ -224,7 +226,7 @@ def getEncyclopedicDataFilm(soup):
         elif infoRow[0].text == "Возраст":
             data["age"] = infoRow[1].text
 
-        return data
+    return data
 
 
 def getActorsFilm(soup):
@@ -282,3 +284,4 @@ if __name__ == "__main__":
     socket.socket = socks.socksocket
 
     parsingTable(1, 3)
+
