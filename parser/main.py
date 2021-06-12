e98c950ed1ef0e9ac883ed7fe30aad6fd1d7feb9
Lemeshev Aleksandr <tivzrrr@gmail.com>
2021-06-12 17:54:58 +0300

    #10: join parser with database

--- parser/main.py
+++ parser/main.py
@@ -4,15 +4,22 @@ import configparser
 from parsingTable import parsingTable
 from parsingFilm import parsingFilm
 
-if __name__ == "__main__":
-    socks.set_default_proxy(socks.SOCKS5, "localhost", 9150)
-    socket.socket = socks.socksocket
+import sys
+sys.path.insert(0, "../films_db")
+import films_db
 
+if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read("settings.ini")
-
+    
     START = int(config["main"]["start"])
     END = int(config["main"]["end"])
+    PORT = int(config["main"]["port"])
+
+    socks.set_default_proxy(socks.SOCKS5, "localhost", PORT)
+    socket.socket = socks.socksocket
+    
+    films_db.init()
 
     for i in range(START, END):
         len_films, ids_films, values_films = parsingTable(i)
@@ -22,3 +29,4 @@ if __name__ == "__main__":
                 data = parsingFilm(ids_films[j])
 
                 print(data)
+                films_db.insert_film(data)
